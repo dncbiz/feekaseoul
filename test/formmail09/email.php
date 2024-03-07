58bd1e8c32a87de6f97629db8a84fb56e5375857
sinnyunki <47134435+sinnyunki@users.noreply.github.com>
2024-03-08 08:44:20 +0900

    re
    re

---
+++ test/formmail09/email.php
@@ -0,0 +1,9 @@
+Input values = <?php print_r($_POST);
+
+$to = "quasidance03@gmail.com";
+$subject = "Form Submission";
+$message = "Name: " . $_POST['name'] . "\nEmail: " . $_POST['email'] . "\nMessage: " . $_POST['message'];
+$headers = "From: " . $_POST['email'];
+mail($to, $subject, $message, $headers);
+header("Location: thank-you.html"); // Redirect to a thank-you page after submission
+?>
